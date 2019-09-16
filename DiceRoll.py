6f562e06ce21af552339aaaccad01e0bd8d789e6
cardo6r <53486754+cardo6r@users.noreply.github.com>
2019-09-16 19:27:08 +0300

    Add files via upload

---
+++ DiceRoll.py
@@ -0,0 +1,9 @@
+import random
+#Using two dice to make it interesting
+x = random.randint(1,6)
+y = random.randint(1,6)
+moves = x+y
+
+print("You rolled a",x,"and you rolled a",y)
+print("You can move",moves,"spaces!")
+    
